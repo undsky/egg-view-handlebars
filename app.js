0d0de98e35684ee55063cce06e93a3be4e448fb4
姜彦汐 <jiangyanxi@live.com>
2021-12-30 12:57:28 +0800

    public

--- app.js
+++ app.js
@@ -1,2 +1,145 @@
-require('bytenode'); 
-module.exports = require('./app.'+process.platform+'.'+process.version.split('.')[0]+'.jsc')
+/*
+ * @Author: 姜彦汐
+ * @Date: 2020-11-27 09:36:21
+ * @LastEditors: 姜彦汐
+ * @LastEditTime: 2021-12-12 12:43:59
+ * @Description: 
+ * @Site: https://www.undsky.com
+ */
+const path = require('path');
+const handlebars = require('handlebars');
+const fs = require('fs');
+
+// https://gist.github.com/akhoury/9118682
+
+handlebars.registerHelper("xif", function (expression, options) {
+  return handlebars.helpers["x"].apply(this, [expression, options]) ? options.fn(this) : options.inverse(this);
+});
+
+handlebars.registerHelper("x", function (expression, options) {
+  var result;
+
+  // you can change the context, or merge it with options.data, options.hash
+  var context = Object.assign(options.data.root, this);
+
+  // yup, i use 'with' here to expose the context's properties as block variables
+  // you don't need to do {{x 'this.age + 2'}}
+  // but you can also do {{x 'age + 2'}}
+  // HOWEVER including an UNINITIALIZED var in a expression will return undefined as the result.
+  with(context) {
+    result = (function () {
+      try {
+        return eval(expression);
+      } catch (e) {
+        console.warn('•Expression: {{x \'' + expression + '\'}}\n•JS-Error: ', e, '\n•Context: ', context);
+      }
+    }).call(context); // to make eval's lexical this=context
+  }
+  return result;
+});
+
+handlebars.registerHelper("link", function (expression, options) {
+  return `<link rel="stylesheet" href="${_getUrl(expression, options)}">`
+});
+
+handlebars.registerHelper("script", function (expression, options) {
+  return `<script src="${_getUrl(expression, options)}"></script>`
+});
+
+module.exports = app => {
+  const {
+    options: compileOptions,
+    runtimeOptions,
+    partialsPath
+  } = app.config.handlebars
+  const prod = 'prod' == app.config.env
+
+  let templates = {}
+  const viewRoot = app.config.view.root
+  const viewsPath = 'string' == typeof viewRoot ? [viewRoot] : viewRoot
+
+  let partials = {}
+  let _partialsPath = [];
+  if (partialsPath) {
+    _partialsPath = 'string' == typeof partialsPath ? [partialsPath] : partialsPath
+  } else {
+    for (const viewPath of viewsPath) {
+      _partialsPath.push(path.join(viewPath, 'partials'))
+    }
+  }
+
+  if (prod) {
+    for (const partialPath of _partialsPath) {
+      _loadPartials(partialPath, partials);
+    }
+    handlebars.registerPartial(partials);
+
+    for (const viewPath of viewsPath) {
+      _loadTemplates(viewPath, _partialsPath, templates)
+    }
+  }
+
+  class HandlebarsView {
+    render(name, locals, options) {
+      let template;
+      if (prod) {
+        template = templates[name]
+      } else {
+        for (const partialPath of _partialsPath) {
+          _loadPartials(partialPath, partials);
+        }
+        handlebars.registerPartial(partials);
+        template = fs.readFileSync(name, 'utf8');
+      }
+      return this.renderString(template, locals, options)
+    }
+
+    renderString(tpl, locals, options) {
+      return handlebars.compile(tpl, compileOptions)(locals, runtimeOptions)
+    }
+  }
+  app.view.use('handlebars', HandlebarsView);
+};
+
+function _loadPartials(partialPath, partials, prefix) {
+  const files = fs.readdirSync(partialPath);
+  for (let file of files) {
+    const filePath = path.join(partialPath, file);
+    const basename = path.basename(filePath, path.extname(filePath))
+    const name = prefix ? prefix + _upperFirst(basename) : basename
+    if (fs.statSync(filePath).isDirectory()) {
+      _loadPartials(filePath, partials, name)
+    } else if ('.html' == path.extname(filePath)) {
+      partials[name] = fs.readFileSync(filePath, 'utf8')
+    }
+  }
+}
+
+function _loadTemplates(viewPath, partialsPath, templates) {
+  const files = fs.readdirSync(viewPath);
+  for (let file of files) {
+    const filePath = path.join(viewPath, file);
+    if (fs.statSync(filePath).isDirectory() && partialsPath.indexOf(filePath) == -1) {
+      _loadTemplates(filePath, partialsPath, templates)
+    } else if ('.html' == path.extname(filePath)) {
+      templates[filePath] = fs.readFileSync(filePath, 'utf8')
+    }
+  }
+}
+
+function _upperFirst(str) {
+  return str.slice(0, 1).toUpperCase() + str.slice(1)
+}
+
+function _getUrl(url, options) {
+  const cdn = options.data.root.cdn;
+  const ts = options.hash.ts ? ('false' == options.hash.ts ? false : true) : options.data.root.ctx.app.config.handlebars.ts;
+  if ('string' == typeof url) {
+    let _url = (url.startsWith('/') || url.startsWith('http')) ? url : (cdn + url)
+    if (ts) {
+      _url += ((_url.indexOf('?') > -1 ? '&' : '?') + 'ts=' + new Date().getTime())
+    }
+    return _url
+  }
+  return url
+}
--- config/config.default.js
+++ config/config.default.js
@@ -1,2 +1,45 @@
-require('bytenode'); 
-module.exports = require('./config.default.'+process.platform+'.'+process.version.split('.')[0]+'.jsc')
+/*
+ * @Author: 姜彦汐
+ * @Date: 2020-11-27 09:33:06
+ * @LastEditors: 姜彦汐
+ * @LastEditTime: 2021-12-12 12:08:18
+ * @Description: https://handlebarsjs.com/zh/
+ * @Site: https://www.undsky.com
+ */
+module.exports = appInfo => ({
+  /**
+   * handlebars default config
+   * More https://handlebarsjs.com/zh/api-reference/compilation.html#handlebars-compile-template-options
+   * @member
+   * @property {Boolean} [data=true] 设置为 false 以终止 @data 跟踪。
+   * @property {Boolean} [compat=true] 设置为 true 以允许递归领域查找。
+   * @property {Boolean} [knownHelpers=false] 将已知在模版运行时会真实存在的助手代码列表 Hash 化。输入本参数会使编译器在一些情况下优化。内置助手代码 已经在本列表里包含，但若设置本项为 false，内置的助手代码可能会被忽视。
+   * @property {Boolean} [knownHelpersOnly=true] 设置为 true 以允许基于已知助手代码列表的进一步优化。
+   * @property {Boolean} [noEscape=false] 设置为 true 以避免 HTML 的内容转义。
+   * @property {Boolean} [strict=false] 在严格模式下运行。在这个模式下，模版将会对缺失参数抛出异常，而非静默忽略。同时，这种模式将会禁止逆操作，比如 {{^foo}}{{/foo}}，除非领域被特意包含在源对象中。
+   * @property {Boolean} [assumeObjects=false] 在遍历路径时，不再检查对象是否存在。这是严格模式的子集，本子集在已知输入安全的情况下会生成最优模版。
+   * @property {Boolean} [preventIndent=true] 默认情况下，缩进的代码片段调用将会导致代码片段整体全部被缩进。在代码片段写入 pre 标签时，这会造成未 预料到的结果。将本项设置成 true 可以避免这种自动缩进的功能。
+   * @property {Boolean} [ignoreStandalone=true] 当设置为 true 时，将不会去除单独的标签。在这种情况下，不在同一行的代码块和代码片段将不会去除本行 里的空格。
+   * @property {Boolean} [explicitPartialContext=true] 对代码片段的上下文进行精确设置。当开启时，没有设置上下文的代码片段将会依托空对象执行。
+   */
+  handlebars: {
+    options: {
+      data: true,
+      compat: true,
+      knownHelpers: true,
+      knownHelpersOnly: false,
+      noEscape: false,
+      strict: false,
+      assumeObjects: false,
+      preventIndent: true,
+      ignoreStandalone: true,
+      explicitPartialContext: true
+    },
+    runtimeOptions: { // https://handlebarsjs.com/zh/api-reference/runtime-options.html
+      allowProtoMethodsByDefault: true,
+      allowProtoPropertiesByDefault: true
+    },
+    partialsPath: null, // 默认在 view.root 对应的 partials 目录下
+    ts: true, // CSS、JavaScript 外部资源引用是否添加时间戳
+  },
+});
